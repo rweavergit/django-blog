a8c649cd97d083b1d726ab8847d3b51be693e8c3
Robert Weaver <r.weaver17751@gmail.com>
2022-05-27 12:08:32 -0400

    Fixed Pages

--- moodyblog/moodyblog/settings.py
+++ moodyblog/moodyblog/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'moodyblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'theblog'/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
